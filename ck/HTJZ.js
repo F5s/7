82b3a9836d3fbc481596a949009681f4b8ed68fe
F5s <68979843+F5s@users.noreply.github.com>
2021-01-02 04:49:37 +0800

    update

--- ck/HTJZ.js
+++ ck/HTJZ.js
@@ -3,9 +3,9 @@ var obj = JSON.parse($response.body);
  "data": {
   "level": 2,
   "status": 1,
-  "openTime": "2099-10-20",
-  "startTime": "2099-10-20",
-  "endTime": "2099-10-20",
+  "openTime": "2019-10-20",
+  "startTime": "2019-10-20",
+  "endTime": "2199-10-20",
   "shareToken": ""
  },
  "code": 0,
--- ck/mubu.js
+++ ck/mubu.js
@@ -5,5 +5,6 @@ https://mubu.com/api/app/user/info
 
 let obj = JSON.parse($response.body);
 
+obj.data.level = 2,
 obj.data.vipEndDate = "21211230"
 $done({body: JSON.stringify(obj)});
