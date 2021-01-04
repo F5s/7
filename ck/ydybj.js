e48e386fdb410473bf9fd3d30912ca170760609f
F5s <68979843+F5s@users.noreply.github.com>
2021-01-04 16:53:19 +0800

    Update ydybj.js

--- ck/ydybj.js
+++ ck/ydybj.js
@@ -24,6 +24,6 @@ obj.properties.isvip = true
 if ($request.url.indexOf(path2) != -1){
   obj.service = 1,
   obj.payYear = true,
-  obj.end = 4811209694
+  obj.end = 4828057694000
  }
 $done({body: JSON.stringify(obj)});
