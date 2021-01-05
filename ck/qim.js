8da0a6f7f6c442912fb9e4da474c54e298961c0a
F5s <68979843+F5s@users.noreply.github.com>
2021-01-06 04:26:15 +0800

    update

--- ck/qim.js
+++ ck/qim.js
@@ -1,31 +1,31 @@
 /*
 qim vip
-https://xiaoshuo.wtzw.com/api/v1/user/get-user-info
-https://xiaoshuo.wtzw.com/api/v1/user/page?
+const path1 = "/get-user-info";
+const path2 = "/user/page";
+const path3 = "/user/my-center";
 
 https://xiaoshuo.wtzw.com/api/v1/user/get-role-adv-vip-info
 
-*/
-
-const path1 = "/get-user-info";
-const path2 = "/user/page";
-const path3 = "/get-role-adv-vip-info";
-
-const url = $request.url;
-
-var obj = JSON.parse($response.body);
-
 if (
 	url.indexOf(path1) != -1 ||
-	url.indexOf(path2) != -1 
+	url.indexOf(path2) != -1 ||
+	url.indexOf(path3) != -1 
 	){
 	obj.data.is_vip = 1
 }
+*/
 
-if ($request.url.indexOf(path3) != -1){
+
+const path4 = "/get-role-adv-vip-info";
+
+var obj = JSON.parse($response.body);
+
+if ($request.url.indexOf(path4) != -1){
 	obj.data.users.isvip = 1,
 	obj.data.users.time = 4811209694000,
 	obj.data.users.isLifetimeVip = 1
+}else {
+	obj.data.is_vip = 1
 }
 
 $done({body: JSON.stringify(obj)});
