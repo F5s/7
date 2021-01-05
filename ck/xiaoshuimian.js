5d6a5be2e94c26d9c140dab0594f8ad858971ca7
F5s <68979843+F5s@users.noreply.github.com>
2021-01-05 23:30:58 +0800

    Update xiaoshuimian.js

--- ck/xiaoshuimian.js
+++ ck/xiaoshuimian.js
@@ -1,8 +1,20 @@
-//*by 
-//obj.point_expires_time = 4811209694
+//*by 黑黑酱
 
-var obj = JSON.parse($response.body);
-    obj.is_vip = 1,
-    obj.vip_expires = 4811209694,
-    obj.point_expires_time = 4811209694
-$done({body: JSON.stringify(obj)});
+re('"is_vip":\\d@"vip_expires":\\d+@"point_expires_time":\\d','"is_vip":1@"vip_expires":4828057694@"point_expires_time":4828057694')
+
+function re() {
+ var body = $response.body;
+ if (arguments[0].includes("@")) {
+  var regs = arguments[0].split("@");
+  var strs = arguments[1].split("@");
+  for (i = 0;i < regs.length;i++) {
+   var reg = new RegExp(regs[i],"g");
+   body = body.replace(reg, strs[i]);
+ }
+}
+ else {
+  var reg = new RegExp(arguments[0],"g");
+  body = body.replace(reg, arguments[1]);
+}
+ $done(body);
+} 
