8be70f3b00d69dcc15e405ae250cd5a4a03289ab
F5s <68979843+F5s@users.noreply.github.com>
2021-01-06 04:44:31 +0800

    Update qim.js

--- ck/qim.js
+++ ck/qim.js
@@ -21,11 +21,14 @@ const path4 = "/get-role-adv-vip-info";
 var obj = JSON.parse($response.body);
 
 if ($request.url.indexOf(path4) != -1){
-	obj.data.users.isvip = 2,
-	obj.data.users.time = 4811209694000,
-	obj.data.users.isLifetimeVip = 1
+	obj.data.default_sex = "1",
+	obj.data.on_line_type = "2",
+	obj.data.rewardVideoConfig.noAdTime = "60",
+	obj.data.users.isvip = "1",
+	obj.data.users.time = "4811209694000",
+	obj.data.users.isLifetimeVip = "1"
 }else {
-	obj.data.is_vip = 1
+	obj.data.is_vip = "1"
 }
 
 $done({body: JSON.stringify(obj)});
