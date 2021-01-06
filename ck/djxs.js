c2ca23ffc356309c9738d57be86e26898c867159
F5s <68979843+F5s@users.noreply.github.com>
2021-01-07 05:06:29 +0800

    Update djxs.js

--- ck/djxs.js
+++ ck/djxs.js
@@ -13,11 +13,11 @@ var obj = JSON.parse($response.body);
 
 if ($request.url.indexOf(path4) != -1){
 	obj.body.cardInfo.title = "特权生效中",
-	obj.body.cardInfo.desc = "2121-01-06 06:52:06到期",
-	obj.body.vipInfo.vipType = 1,
+	obj.body.cardInfo.desc = "2121-01-06 到期",
+	obj.body.vipInfo.vipType = 2,
 	obj.body.vipInfo.vipExpire = 4811209694000
 }else {
-	obj.body.userInfo.vipInfo.vipType = 1,
+	obj.body.userInfo.vipInfo.vipType = 2,
 	obj.body.userInfo.vipInfo.vipExpire = 4811209694.000,
 	obj.body.userInfo.vipInfo.expireDate = "2122-06-18",
 	obj.body.userInfo.vipInfo.content = "特权到期：2122-06-18",
