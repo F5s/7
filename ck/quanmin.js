781c138ec28b09265bcabfb35fc68fa291706950
F5s <68979843+F5s@users.noreply.github.com>
2021-01-09 01:08:31 +0800

    update

--- ck/quanmin.js
+++ ck/quanmin.js
@@ -7,11 +7,19 @@ quanmin vip
 
 var obj = JSON.parse($response.body);
 
-	obj.data.ad_style = 0,
-	obj.data.ad_end_datetime = "1970-01-01 23:59:59",
+if ($request.url.indexOf(path1) != -1){
+	obj.data.ad.ad_style = 1,
+	obj.data.ad.ad_end_datetime = "2122-01-01 23:59:59",
+	obj.data.ad.ad_state = 1,
+	obj.data.ad.serv_datetime = "2021-01-09 00:56:51",
+	obj.data.ad.ad_tips = "已开通"
+} else {
+	obj.data.ad_style = 1,
+	obj.data.ad_end_datetime = "2122-01-01 23:59:59",
 	obj.data.ad_state = 1,
-	obj.data.serv_datetime = "2122-01-01 23:59:59",
+	obj.data.serv_datetime = "2021-01-09 00:56:51",
 	obj.data.ad_tips = "已开通"
+}
 
 
 $done({body: JSON.stringify(obj)});
