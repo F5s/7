2f6d317ed9717fe6ef0aedc88afd508aba299d01
F5s <68979843+F5s@users.noreply.github.com>
2021-01-09 00:56:25 +0800

    update

--- ck/quanmin.js
+++ ck/quanmin.js
@@ -7,8 +7,8 @@ quanmin vip
 
 var obj = JSON.parse($response.body);
 
-	obj.data.ad_style = 1,
-	obj.data.ad_end_datetime = "2122-01-01 23:59:59",
+	obj.data.ad_style = 0,
+	obj.data.ad_end_datetime = "1970-01-01 23:59:59",
 	obj.data.ad_state = 1,
 	obj.data.serv_datetime = "2122-01-01 23:59:59",
 	obj.data.ad_tips = "已开通"
