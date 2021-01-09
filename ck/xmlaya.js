d74911223e9a55c5366ffcb6fde92cdfc36a7c52
F5s <68979843+F5s@users.noreply.github.com>
2021-01-10 07:39:14 +0800

    Update xmlaya.js

--- ck/xmlaya.js
+++ ck/xmlaya.js
@@ -9,6 +9,7 @@ djxs vip
 
 
 var body = $response.body;
+const path1 = "/homePage";
 
 if ($request.url.indexOf(path1) != -1){
 body = body.replace(/"isVip":false/g,'"isVip":true').replace(/"vipExpireTime":\d*/g,'"vipExpireTime":4811209694000').replace(/"vipStatus":\d/g,'"vipStatus":5');
