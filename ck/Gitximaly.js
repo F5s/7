2b2de7b7b42e206967589d7a90a5c349984caed0
F5s <68979843+F5s@users.noreply.github.com>
2021-01-11 05:05:56 +0800

    update git

--- ck/Gitximaly.js
+++ ck/Gitximaly.js
@@ -9,20 +9,20 @@ djxs vip
 
 
 var body = $response.body;
-var url = $request.url;
+var uurl = $request.url;
 const path1 = "/homePage";
 const path2 = "/mobile/playlist";
 const path3 = "/track/pay";
 const path4 = "/v2/baseInfo";
 
 
-if (url.indexOf(path1) != -1){
+if (uurl.indexOf(path1) != -1){
 body = body.replace(/"isVip":false/g,'"isVip":true').replace(/"vipExpireTime":\d*/g,'"vipExpireTime":4811209694000').replace(/"vipStatus":\d/g,'"vipStatus":5');
 }
 if (
-	url.indexOf(path2) != -1 ||
-	url.indexOf(path3) != -1 ||
-	url.indexOf(path4) != -1 
+	uurl.indexOf(path2) != -1 ||
+	uurl.indexOf(path3) != -1 ||
+	uurl.indexOf(path4) != -1 
 	){
 body = body.replace(/"isFree":false/g,'"isFree":true').replace(/rice":\d*.\d*/g,'rice":0.0').replace(/"isAutoBuy":false/g,'"isAutoBuy":true').replace(/"hqNeedVip":false/g,'"hqNeedVip":true').replace(/"isVipFree":false/g,'"isVipFree":true').replace(/"vipFreeType":\d/g,'"vipFreeType":1');
 }
