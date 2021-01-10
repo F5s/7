728eb373bd745de00464182a94cf818abc3fce9f
F5s <68979843+F5s@users.noreply.github.com>
2021-01-11 06:14:50 +0800

    404

---
+++ ck/Gitxmly.js
@@ -0,0 +1,23 @@
+/*
+lya vip
+/mobile/homePage/
+*/
+
+
+const path1 = "/mobile/homePage";
+
+
+var obj = JSON.parse($response.body);
+if ($request.url.indexOf(path1) != -1){
+	obj.userGrade = 3,
+	obj.isVip = true,
+	obj.xiaoyaGrade = 3,
+	obj.vipStatus = 3,
+	obj.userTitle = "秀才",
+	obj.vipLevel = 3,
+	obj.xiaoyaGradeName = "秀才1级",
+	obj.vipExpireTime = 4811209694000
+}
+
+
+$done({body: JSON.stringify(obj)});
