bfdb0515e0b2fdec8a88ecd4ad3e8ddeb44179b8
F5s <68979843+F5s@users.noreply.github.com>
2021-01-11 04:06:51 +0800

    Update ximaly.js

--- ck/ximaly.js
+++ ck/ximaly.js
@@ -9,11 +9,8 @@ djxs vip
 
 
 var body = $response.body;
-const path1 = "/homePage";
-
-if ($request.url.indexOf(path1) != -1){
 body = body.replace(/"isVip":false/g,'"isVip":true').replace(/"vipExpireTime":\d*/g,'"vipExpireTime":4811209694000').replace(/"vipStatus":\d/g,'"vipStatus":5');
-}
+
 
 body = body.replace(/"isFree":false/g,'"isFree":true').replace(/rice":\d*.\d*/g,'rice":0.0').replace(/"isAutoBuy":false/g,'"isAutoBuy":true').replace(/"hqNeedVip":false/g,'"hqNeedVip":true').replace(/"isVipFree":false/g,'"isVipFree":true').replace(/"vipFreeType":\d/g,'"vipFreeType":1');
 
