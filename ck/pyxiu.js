6d4f9a9d483a4b9d32da9d7181c8c33ec032fd03
F5s <68979843+F5s@users.noreply.github.com>
2021-01-13 00:24:49 +0800

    add pyxiu

---
+++ ck/pyxiu.js
@@ -0,0 +1,19 @@
+/*
+pyxiu
+
+*/
+
+const path1 = "/GetMyInfo";
+
+var obj = JSON.parse($response.body);
+obj.data.is_vip = 1
+if ($request.url.indexOf(path1) != -1){
+obj.data.gold = 9999,
+obj.data.gold2 = 9999
+}else {
+obj.data.end_time = 4811209694000,
+obj.data.vipLevel = 3,
+obj.data.eventEndText = "2122-06-18"
+}
+
+$done({body: JSON.stringify(obj)});
