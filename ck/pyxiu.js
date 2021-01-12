ea9ab64cee0b84a47a008b54f73b0e60203e245b
F5s <68979843+F5s@users.noreply.github.com>
2021-01-13 01:10:00 +0800

    update

--- ck/pyxiu.js
+++ ck/pyxiu.js
@@ -10,7 +10,7 @@ obj.data.is_vip = 1
 if ($request.url.indexOf(path1) != -1){
 obj.data.gold = 9999,
 obj.data.gold2 = 9999
-}else {
+} else {
 obj.data.end_time = 4811209694000,
 obj.data.vipLevel = 3,
 obj.data.eventEndText = "2122-06-18"
