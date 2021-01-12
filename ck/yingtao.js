14f8e0d0856545bc61f86c216b60127f9da6bb02
F5s <68979843+F5s@users.noreply.github.com>
2021-01-13 07:48:58 +0800

    update

--- ck/yingtao.js
+++ ck/yingtao.js
@@ -4,10 +4,10 @@ yingtao vip
 */
 
 var body = $response.body;
-body = body.replace(/isAd\":\\w+/g,'isAd\":false');
-body = body.replace(/"vipType\":\\w+/g,'vipType\":1');
-body = body.replace(/"vipEndTime\":\\w+/g,'vipEndTime\":\"4811209694\"');
-body = body.replace(/"userLevel\":\\d/g,'userLevel\":8');
+body = body.replace(/isAd":\w+/g,'isAd":false');
+body = body.replace(/vipType":\w+/g,'vipType":1');
+body = body.replace(/vipEndTime":\w+/g,'vipEndTime":4811209694');
+body = body.replace(/userLevel":\d/g,'userLevel":8');
 
 
 $done({ body });
