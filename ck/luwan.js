20512831f6647b7c994b5eccd41285f2be87272f
F5s <68979843+F5s@users.noreply.github.com>
2021-01-13 08:48:15 +0800

    Update luwan.js

--- ck/luwan.js
+++ ck/luwan.js
@@ -5,6 +5,7 @@ luwan vip
 
 
 var obj = JSON.parse($response.body);
-obj.data.expires_date_ms = 4811209694000
+obj.data.expires_date_ms = 4811209694000,
+obj.data.user_type = "vip"
 
 $done({body: JSON.stringify(obj)});
