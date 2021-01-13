a55bd1cd05f1b3789a6d64b1942eb805ea0b26cc
F5s <68979843+F5s@users.noreply.github.com>
2021-01-13 16:18:39 +0800

    update

--- ck/aiqiyyd.js
+++ ck/aiqiyyd.js
@@ -2,7 +2,7 @@
 aiqiyyd vip
 
 HTTPS	passport.iqiyi.com	/apis/user/info.action	
-HTTP	i.vip.iqiyi.com	/client/store
+HTTP	i.vip.iqiyi.com	/client/store/ios/mixMultiProducts.action
 HTTPS	api-yuedu-1.iqiyi.com	/book/monthly/popToast
 HTTPS	api-yuedu-1.iqiyi.com	/book/member/userInfo
 HTTPS	api-yuedu-1.iqiyi.com	/book/cloudStrategy
@@ -13,60 +13,62 @@ HTTPS	api-yuedu.iqiyi.com		/book/cloudStrategy
 
 
 var obj = JSON.parse($response.body);
+var uurl = $request.url;
 const path1 = "/user/info.action";
-const path2 = "/client/store";
+const path2 = "/client/store/ios/mixMultiProducts.action";
 const path3 = "/monthly/popToast";
 const path4 = "/member/userInfo";
 const path5 = "/book/cloudStrategy";
 
-if ($request.url.indexOf(path1) != -1){
-obj.data.vip_list[0].level = 4,
-obj.data.vip_list[0].paidSign = 4,
-obj.data.vip_list[0].vipType = 4,
-obj.data.vip_list[0].payType = 4,
-obj.data.vip_list[0].status = 4,
-obj.data.vip_list[0].type = 4,
-obj.data.vip_list[0].surplus = 4,
-obj.data.vip_list[0].yearExpire = 4,
+if (uurl.indexOf(path1) != -1){
+obj.data.vip_list[0].level = "1",
+obj.data.vip_list[0].paidSign = 1,
+obj.data.vip_list[0].vipType = "1",
+obj.data.vip_list[0].payType = "0",
+obj.data.vip_list[0].status = "1",
+obj.data.vip_list[0].type = "1",
+obj.data.vip_list[0].surplus = "1",
+obj.data.vip_list[0].yearExpire = 1,
 obj.data.vip_list[0].deadline.t = 4811209694000,
 obj.data.vip_list[0].longestDeadline.t = 4811209694000
 }
 
-if ($request.url.indexOf(path2) != -1){
-obj.data.storeInfo[0].vipType = 4,
+if (uurl.indexOf(path2) != -1){
+obj.data.storeInfo[0].vipType = 1,
 obj.data.storeInfo[0].isValidVip = true,
 obj.data.userInfo.expire = 0,
 obj.data.userInfo.vipDeadline = "2122-06-18 15:08:14",
 obj.data.userInfo.isVipUser = true,
-obj.data.userInfo.status = 4
+obj.data.userInfo.status = 1
 }
 
 
-if ($request.url.indexOf(path3) != -1){
+if (uurl.indexOf(path3) != -1){
 obj.data.topCapacityEndTime = 4811209694000,
-obj.data.isDiamondMonthlyMember = "4",
+obj.data.isDiamondMonthlyMember = "0",
 obj.data.memberExpireGuideText = "终身会员",
-obj.data.topCapacity = 4,
-obj.data.showDiamondVipLabel = "1",
+obj.data.topCapacity = 2,
+obj.data.showDiamondVipLabel = "0",
 obj.data.monthlyEndTime = 4811209694000,
 obj.data.isMonthlyMember = "1"
 }
 
-if ($request.url.indexOf(path4) != -1){
-obj.user_info.vip_info[0].vipType = 4,
+if (uurl.indexOf(path4) != -1){
+obj.user_info.vip_info[0].vipType = 2,
 obj.user_info.vip_info[0].startTime = 1610513251292,
 obj.user_info.vip_info[0].endTime = 4811209694000,
-obj.user_info.vip_info[0].text = "星钻会员至 2122-06-18",
-obj.user_info.vip[0].vipType = "4",
+obj.user_info.vip_info[0].text = "黄金会员至 2122-06-18",
+obj.user_info.vip[0].vipType = "1",
 obj.user_info.vip[0].startTime = 1610513251292,
-obj.user_info.type = 4,
+obj.user_info.vip[0].endTime = 4811209694000,
+obj.user_info.type = 1,
 obj.user_info.deadline = "2122-06-18"
 }
 
-if ($request.url.indexOf(path5) != -1){
+if (uurl.indexOf(path5) != -1){
 obj.monthlyMemberEndTime = 4811209694000,
 obj.isMonthlyMember = true,
-obj.monthlyMember.baseVipType = 4,
+obj.monthlyMember.baseVipType = 1,
 obj.monthlyMember.baseVipEndTime = 4811209694000,
 obj.monthlyMember.baseVipStartTime = 1610513251292,
 obj.monthlyMember.isVipExpire = false,
