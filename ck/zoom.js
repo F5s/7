8c4294f3214bb2cfbac190a114999ca16b376604
F5s <68979843+F5s@users.noreply.github.com>
2021-01-15 02:28:40 +0800

    Update zoom.js

--- ck/zoom.js
+++ ck/zoom.js
@@ -10,23 +10,18 @@ zoome vip
 
 var obj = JSON.parse($response.body);
 var uurl = $request.url;
-const path1 = "/user/update";
-const path2 = "/user/profile";
-const path2 = "/v1/subscribers";
+const path1 = "/v1/subscribers";
 
 
-if (uurl.indexOf(path1) != -1 ||
-	uurl.indexOf(path2) != -1 ){
-obj.result.is_pro = true,
-obj.result.pro_expiry_date = "4811209694000",
-}
-
-if (uurl.indexOf(path3) != -1){
+if ($request.url.indexOf(path1) != -1){
 obj.subscriber.entitlements.pro.expires_date = "2122-06-18T15:08:14Z",
 obj.subscriber.entitlements.pro.grace_period_expires_date = "2122-06-18T15:08:14Z",
 obj.subscriber.subscriptions.zoomerang.yearly.grace_period_expires_date = "2122-06-18T15:08:14Z",
 obj.subscriber.subscriptions.zoomerang.yearly.expires_date = "2122-06-18T15:08:14Z",
-obj.subscriber.subscriptions.zoomerang.yearly.period_type = "yearly",
+obj.subscriber.subscriptions.zoomerang.yearly.period_type = "yearly"
+} else {
+obj.result.is_pro = true,
+obj.result.pro_expiry_date = "4811209694000"
 }
 
 $done({body: JSON.stringify(obj)});
