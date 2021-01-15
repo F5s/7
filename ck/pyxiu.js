341e481dedbebf61f3bf0861563b905950cf228b
F5s <68979843+F5s@users.noreply.github.com>
2021-01-15 09:52:55 +0800

    Update pyxiu.js

--- ck/pyxiu.js
+++ ck/pyxiu.js
@@ -9,8 +9,8 @@ const path1 = "/GetMyInfo";
 var obj = JSON.parse($response.body);
 obj.data.is_vip = 1
 if ($request.url.indexOf(path1) != -1){
-obj.data.gold = 9999,
-obj.data.gold2 = 9999
+obj.data.gold = 99999,
+obj.data.gold2 = 99999
 } else {
 obj.data.end_time = "2122-06-18 15:08:14",
 obj.data.vipLevel = 3,
