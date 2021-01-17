67067066fab28268292f3c8544b1749c11ba5ded
F5s <68979843+F5s@users.noreply.github.com>
2021-01-18 03:36:34 +0800

    add vochihf

---
+++ ck/vochihf.js
@@ -0,0 +1,22 @@
+let obj = JSON.parse($response.body);
+obj["subscriber"]["subscriptions"]["vochi_yearly_sale"]= 
+{
+"is_sandbox": false,
+"period_type": "active",
+"billing_issues_detected_at": "2020-12-22T07:04:02Z",
+"unsubscribe_detected_at": null,
+"expires_date": "2121-01-07T05:20:23Z",
+"grace_period_expires_date": "2121-01-07T05:20:23Z",
+"original_purchase_date": "2020-12-19T05:20:24Z",
+"purchase_date": "2020-12-19T05:20:23Z",
+"store": "app_store"
+};
+obj["subscriber"]["entitlements"] = {
+"pro": {
+"grace_period_expires_date": "2121-01-07T05:20:23Z",
+"purchase_date": "2020-12-19T05:20:23Z",
+"product_identifier": "vochi_yearly_sale",
+"expires_date": "2121-01-07T05:20:23Z"
+  }
+};
+$done({body: JSON.stringify(obj)});
