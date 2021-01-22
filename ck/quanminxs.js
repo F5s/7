55cef53f96d6ba5360ab874af39684e85e8a7abe
F5s <68979843+F5s@users.noreply.github.com>
2021-01-22 19:46:40 +0800

    update

--- ck/quanminxs.js
+++ ck/quanminxs.js
@@ -12,13 +12,11 @@ if ($request.url.indexOf(path1) != -1){
 	obj.data.ad.ad_style = 1,
 	obj.data.ad.ad_state = 1,
 	obj.data.ad.ad_end_datetime = "2122-01-01 23:59:59",
-	obj.data.ad.serv_datetime = "2021-01-09 00:56:51",
 	obj.data.ad.ad_tips = "已开通"
 }else {
 	obj.data.ad_style = 1,
 	obj.data.ad_state = 1,
 	obj.data.ad_end_datetime = "2122-01-01 23:59:59",
-	obj.data.serv_datetime = "2021-01-09 00:56:51",
 	obj.data.ad_tips = "已开通"
 }
 
