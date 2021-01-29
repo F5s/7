65c0731b2bcf7fd7240d38f3b6d8a75ccf8a2bf6
F5s <68979843+F5s@users.noreply.github.com>
2021-01-29 14:18:05 +0800

    update

--- ck/rbang.js
+++ ck/rbang.js
@@ -6,8 +6,6 @@ rbang vip
 
 var obj = JSON.parse($response.body);
 
-	obj.data.is_vip_now = true,
-	obj.data.vip_expired = 4811209694,
-	obj.data.is_vip = 1
+obj.data = {"is_vip":"1","vip_expired":"4811209694","is_vip_now":true}
 
 $done({body: JSON.stringify(obj)});
