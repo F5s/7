be0694c28c8f720b8ee1896327848488f4d011b4
F5s <68979843+F5s@users.noreply.github.com>
2021-01-30 17:45:50 +0800

    update

--- ck/wxyd.js
+++ ck/wxyd.js
@@ -3,13 +3,17 @@ wxyd vip
 /pay/memberCardSummary
 */
 
-
+const path1 = "/user/profile";
 var obj = JSON.parse($response.body);
+
 obj.startTime = 1611572535,
 obj.expiredTime = 4769942399,
 obj.expired = 0,
 obj.isPaying = 0,
 obj.permanent = 1,
 obj.remainTime = 4769942399
-
+if ($request.url.indexOf(path1) != -1){
+obj.isNewDevice = true,
+obj.isSubscribing = 1
+}
 $done({body: JSON.stringify(obj)});
