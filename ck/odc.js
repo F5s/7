2fff7011826c562c5796cb96cd07da04d66006dd
F5s <68979843+F5s@users.noreply.github.com>
2021-01-30 13:37:19 +0800

    Update odc.js

--- ck/odc.js
+++ ck/odc.js
@@ -5,5 +5,5 @@ odc
 
 var body = $response.body;
 
-body = 'jsonFeed({"country":"US","state":"CA","stateName":"California","zipcode":"94536","timezone":"America/Los_Angeles","latitude":"37.56250","longitude":"-122.00040","city":"Fremont","continent":"NA"});';
+body = jsonFeed({"country":"US","state":"CA","stateName":"California","zipcode":"94536","timezone":"America/Los_Angeles","latitude":"37.56250","longitude":"-122.00040","city":"Fremont","continent":"NA"});
 $done({ body });
