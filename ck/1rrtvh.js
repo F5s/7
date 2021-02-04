a6a59a75dbaf72241f85320ec56d6a2e6ffc7896
F5s <68979843+F5s@users.noreply.github.com>
2021-02-04 20:45:59 +0800

    Update 1rrtvh.js

--- ck/1rrtvh.js
+++ ck/1rrtvh.js
@@ -1,5 +1,6 @@
 var modifiedHeaders = $request.headers;
 modifiedHeaders['clientVersion'] = '4.18.1';
 modifiedHeaders['User-Agent'] = 'PUClient/4.18.1 (iPhone; iOS 14.2; Scale/3.00)';
+modifiedHeaders['Cookie'] = 'JSESSIONID=BB2A512763EBEB452ECE8556C1C15197';
 
 $done({headers : modifiedHeaders});
