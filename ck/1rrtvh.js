417d553cc2a793904d575643743d6d38afe720bd
F5s <68979843+F5s@users.noreply.github.com>
2021-02-04 16:53:52 +0800

    Update 1rrtvh.js

--- ck/1rrtvh.js
+++ ck/1rrtvh.js
@@ -1,5 +1,5 @@
 var modifiedHeaders = $request.headers;
-modifiedHeaders['clientVersion'] = '5.0.1';
-modifiedHeaders['User-Agent'] = 'PUClient/5.0.1 (iPhone; iOS 14.2; Scale/3.00)';
+modifiedHeaders['clientVersion'] = '4.18.1';
+modifiedHeaders['User-Agent'] = 'PUClient/4.18.1 (iPhone; iOS 14.2; Scale/3.00)';
 
 $done({headers : modifiedHeaders});
