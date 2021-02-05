2c26cfb265d4057a5455c33e6b81143d93a39d03
F5s <68979843+F5s@users.noreply.github.com>
2021-02-06 02:46:35 +0800

    update

--- ck/efekt.js
+++ ck/efekt.js
@@ -1,9 +1,12 @@
 /*
 efekt
 */
-let obj = JSON.parse($response.body);
-obj.data.results.subscriptions[0].status = "active";
-obj.data.results.subscriptions[0].expires_at = "2121-02-05T12:26:42.630Z";
-obj.data.results.subscriptions[0].units_count = 100;
-obj.data.results.subscriptions[0].active_till = "2121-02-05T12:26:42.630Z";
-$done({body: JSON.stringify(obj)});
+let body = $response.body;
+body = body.replace(/status":"expired"/g,'status":"active"');
+body = body.replace(/autorenew_enabled":false/g,'autorenew_enabled":true');
+body = body.replace(/expires_at":".*Z"/g,'expires_at":"2121-02-05T12:26:42.630Z"');
+body = body.replace(/active_till":".*Z"/g,'active_till":"2121-02-05T12:26:42.630Z"');
+
+$done({
+	body
+});
