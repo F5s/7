f6350fec43247f32cc18ab1e8d431fb4fb7b0b9b
F5s <68979843+F5s@users.noreply.github.com>
2021-02-05 19:50:46 +0800

    Update buyitunes.js

--- ck/buyitunes.js
+++ ck/buyitunes.js
@@ -1,11 +1,11 @@
 let body= $response.body;
 let obj = JSON.parse(body);
 if (body.indexOf("expires") != -1) {
-  obj["receipt"]["in_app"][0]["expires_date"] = "2099-10-19 05:14:18 Etc/GMT";
-  obj["receipt"]["in_app"][0]["expires_date_pst"] = "2099-10-18 22:14:18 America/Los_Angeles";
-  obj["receipt"]["in_app"][0]["expires_date_ms"] = "4096019658000";
-  obj["latest_receipt_info"][0]["expires_date"] = "2099-10-19 05:14:18 Etc/GMT";
-  obj["latest_receipt_info"][0]["expires_date_pst"] = "2099-10-18 22:14:18 America/Los_Angeles";
-  obj["latest_receipt_info"][0]["expires_date_ms"] = "4096019658000";
+  obj["receipt"]["in_app"][0]["expires_date"] = "2122-06-18 15:08:14 Etc/GMT";
+  obj["receipt"]["in_app"][0]["expires_date_pst"] = "2122-06-18 15:08:14 America/Los_Angeles";
+  obj["receipt"]["in_app"][0]["expires_date_ms"] = "4811209694000";
+  obj["latest_receipt_info"][0]["expires_date"] = "2122-06-18 15:08:14 Etc/GMT";
+  obj["latest_receipt_info"][0]["expires_date_pst"] = "2122-06-18 15:08:14 America/Los_Angeles";
+  obj["latest_receipt_info"][0]["expires_date_ms"] = "4811209694000";
 }
 $done({body: JSON.stringify(obj)});
