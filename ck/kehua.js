7c1ffdfbdea1a052ca466f3e6f118e319506ffd9
F5s <68979843+F5s@users.noreply.github.com>
2021-02-06 20:59:53 +0800

    update

--- ck/kehua.js
+++ ck/kehua.js
@@ -1,6 +1,17 @@
 /*
-/api/v2/sfo/user_popup_configs/UAEVW3UJ2s0?
+https://www.canva.cn/_ajax/subscription/subscriptions?
 */
-var obj = JSON.parse($response.body);
-obj.popup_plans[0].expire_at = 4811209694000;
-$done({body: JSON.stringify(obj)});
+
+const path1 = "_ajax/subscription/subscriptions";
+
+var body = $response.body;
+var url = $request.url;
+
+if (url.indexOf(path1) != -1) {
+	body = body.replace(/subscriptions\"\:\[\{.*/,'subscriptions":[{"id":"410791","plan":"16","owningBrand":"BAEVW_MaA6o","creatingBrand":"BAEVW_MaA6o","creatingUser":"UAEVW3UJ2s0","status":"TRIALING","pastDue":false,"quantity":1,"paymentConfig":{"A?":"C","C":"YEAR","D":1},"createdDate":1612613603000,"featureAccessDate":1612613602000,"currentPeriodStartDate":1612613602000,"currentPeriodEndDate":4811209694000,"trialPeriodEndDate":4811209694000,"cancelAtPeriodEnd":false,"periodEndAction":"C","provider":"APPLE","planDetails":{"id":"16","name":"Canva for Business","descriptor":"CANVA_FOR_BUSINESS","description":"Low June 2020 Prices","featureBundle":"CBUS","trialPeriodDays":99999,"trialsPerBrand":2,"subscriberType":"BRAND","legacy":false,"userLocked":false,"free":false,"graceEnabled":true},"externalId":"470000793037582"}]}');
+}
+
+$done({
+	body
+});
+
