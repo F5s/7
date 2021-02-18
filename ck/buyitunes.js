c445b40b8da1b31b205916ebcf6a9bc66d98f6bd
F5s <68979843+F5s@users.noreply.github.com>
2021-02-19 07:14:45 +0800

    Update buyitunes.js

--- ck/buyitunes.js
+++ ck/buyitunes.js
@@ -1,11 +1,11 @@
 let body= $response.body;
 let obj = JSON.parse(body);
-/*if (body.indexOf("expires") != -1) {}*/
+if (body.indexOf("expires") != -1) {
 obj["receipt"]["in_app"][0]["expires_date"] = "2122-06-18 15:08:14 Etc/GMT";
 obj["receipt"]["in_app"][0]["expires_date_pst"] = "2122-06-18 15:08:14 America/Los_Angeles";
 obj["receipt"]["in_app"][0]["expires_date_ms"] = "4811209694000";
 obj["latest_receipt_info"][0]["expires_date"] = "2122-06-18 15:08:14 Etc/GMT";
 obj["latest_receipt_info"][0]["expires_date_pst"] = "2122-06-18 15:08:14 America/Los_Angeles";
 obj["latest_receipt_info"][0]["expires_date_ms"] = "4811209694000";
-
+}
 $done({body: JSON.stringify(obj)});
