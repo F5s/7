eedebf7d5e690cac8f4f9446c96f7de715e7806d
F5s <68979843+F5s@users.noreply.github.com>
2021-03-08 10:39:05 +0800

    add

--- ck/waidd.js
+++ ck/waidd.js
@@ -5,15 +5,16 @@ waidd
 const path1 = "/user/member";
 var obj = JSON.parse($response.body);
 if ($request.url.indexOf(path1) != -1) {
-obj.data.recharge_level = 9,
-obj.data.customer_type = 2,
-obj.data.is_agent = 0,
+obj.data.recharge_level = 99,
+obj.data.customer_type = 3,
+obj.data.is_agent = 1,
 obj.data.is_member = 1,
 obj.data.expire_at = "2122-06-18 15:08:14",
 obj.data.remaining_days = 9999,
-obj.data.if_is_agent = false
+obj.data.if_is_agent = true
 }else{
-obj.data.expire = 4811209694
+obj.data.expire = 4811209694,
+obj.data.is_member = true
 }
 
 $done({body: JSON.stringify(obj)});
