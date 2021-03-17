b5f4178a3ced6bd33d3a3e303a0548109e189b28
F5s <68979843+F5s@users.noreply.github.com>
2021-03-18 02:21:28 +0800

    Update qds.js

--- ck/qds.js
+++ ck/qds.js
@@ -3,9 +3,13 @@ qds
 */
 
 var obj = JSON.parse($response.body);
+if (obj.data.is_buy != 1) {
 obj.data.is_buy = 1,
 obj.data.subscribe_price = "0.00",
-obj.data.resource.is_free = 1
+obj.data.resource.is_free = 1,
+obj.data.resource.detail[0].content = obj.data.resource.detail[0].sub_content
+}
+
 
 $done({body: JSON.stringify(obj)});
 
