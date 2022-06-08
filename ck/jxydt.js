07fe7858e87cb14a4958aa4628437c9c7128485c
F5s <68979843+F5s@users.noreply.github.com>
2022-06-08 20:01:25 +0800

    Update jxydt.js

--- ck/jxydt.js
+++ ck/jxydt.js
@@ -1,3 +1,12 @@
+
+//
+/*
+# 驾照一点通vip
+QX1.0.0:
+^https:\/\/vipapi\.jxedt\.com\/vip\/check url script-response-body https://raw.githubusercontent.com/zgaoliang520/zijianyong/master/jxydt.js
+Surge4.0:
+http-response ^https://vipapi.jxedt.com/vip/ requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/zgaoliang520/zijianyong/master/jxydt.js,script-update-interval=0
+MITM = vipapi.jxedt.com
 var obj = JSON.parse($response.body);
  obj= {
  "code": 0,
@@ -17,6 +26,19 @@ var obj = JSON.parse($response.body);
   "vipResidualDay": 999,
   "imeiVIPOrderBindStatus": 1
  }
+};
+$done({body: JSON.stringify(obj)});
+*/
+
+var obj = JSON.parse($response.body);
+ obj={
+ "data": {
+  "is_vip": "1",
+  "vip_expired": "4092599349",
+  "is_vip_now": true
+ },
+ "error": false,
+ "status": 200
 };
 $done({body: JSON.stringify(obj)});
 //
